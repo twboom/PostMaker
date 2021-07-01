959e37553a4d512630d7e9585e1a7da947ed575e
Thijs Boom <twboom@hotmail.nl>
2021-07-01 23:24:10 +0200

    Almost finished export function

--- js/render.js
+++ js/render.js
@@ -1,13 +1,25 @@
 // Renderer for exporting
 class Renderer {
 
-    constructor(post) {}
+    constructor(post, canvas) {
+
+        this.post = post;
+        this.canvas = canvas;
+        this.context = canvas.getContext('2d');
+
+    }
 
     // Rendering
-    render(html, ctx, x, y, width, height) {
+    render(x, y, width, height) {
+
+        const html = this.post;
+        const ctx = this.context
+
+        // Following code comes from https://stackoverflow.com/a/43724114/14445654
+
         var data = "data:image/svg+xml;charset=utf-8," + '<svg xmlns="http://www.w3.org/2000/svg" width="' + width + '" height="' + height + '">' +
             '<foreignObject width="100%" height="100%">' +
-            html_to_xml(html) +
+            this.html_to_xml(html) +
             '</foreignObject>' +
             '</svg>';
 
@@ -18,9 +30,26 @@ class Renderer {
         img.src = data;
     }
 
+    // Export it as an image
+    export(type) {
+
+        const canvas = this.canvas
+        console.log(canvas.toDataURL('image/jpeg'))
+
+        const download = document.createElement('a');
+        download.href = canvas.toDataURL(`image/${type}`)
+        download.download = 'PostMaker';
+        download.innerText = 'Donwload!'
+        document.body.appendChild(download)
+        
+    }
+
     // Utitlity code
     html_to_xml(html) {
-        // This code comes from https://stackoverflow.com/a/43724114/14445654
+
+        html = html.innerHTML;
+
+        // Following code comes from https://stackoverflow.com/a/43724114/14445654
 
         const doc = document.implementation.createHTMLDocument('');
         doc.write(html);
